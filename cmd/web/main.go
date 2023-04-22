0ff1bdcb122cd28092869b3e2cbea0a4ae87baff
Heaven Chen <heavenchen@qnap.com>
2023-04-22 14:42:28 +0800

    Init project and install depedency

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to the database
+
+	// create sessions
+
+	// create waitgroup
+
+	// set up the application config
+
+	// set up mail
+
+	// lisetn for web connections
+}
